852ab9072224bb97c9602b19d33ba5101077db06
benttol <75014765+benttol@users.noreply.github.com>
2020-12-15 04:45:33 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,24 @@
+const help = () => {
+	return '> ```Semoga hari ini lebih baik dari hari kemarin.``` <\n\n' +
+	'> ```Sticker Commands``` <\n' +
+	'Command : *#sticker* atau *#stiker*\n' +
+	'Deskripsi : mengubah foto/gif/video menjadi sticker\n' +
+	'Contoh : reply foto/gif/video, atau kirim foto/gif/video bersama caption\n\n' + 
+	'Command : *#toimg*\n' +
+	'Deskripsi : mengubah sticker menjadi foto\n' +
+	'Contoh : reply sticker\n\n' +
+	'Command : *#tsticker* atau *#tstiker*\n' +
+	'Deskripsi : mengubah teks menjadi sticker\n' +
+	'Contoh : *#tsticker hallo*\n\n' +
+	'> ```Meme Commands``` <\n' +
+	'Command : *#meme*\n' +
+	'Deskripsi : random meme images [english]\n' +
+	'Contoh : *kirim saja perintahnya*\n\n' +
+	'> ```Google Text-To-Speech Commands``` <\n' +
+	'Command : *#gtts*\n' +
+	'Deskripsi : *mengubah teks menjadi speech/audio*\n' +
+	'Contoh : *#gtts [cc] [text]*\nexample : *#gtts id Bentol Ganteng*\n\n'
+	'> ```Powered By Pudidi``` <'
+}
+
+exports.help = help
