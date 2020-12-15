dce4ddee06530c6da12ff9cb69cdde673493dd3d
benttol <75014765+benttol@users.noreply.github.com>
2020-12-15 16:30:12 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,25 @@
+const help = () => {
+	return 'Sticker Commands\n' +
+	'Perintah : *#sticker* or *#stiker*\n' +
+	'Deskripsi : Merubah foto/gif/video menjadi sticker\n' +
+	'Contoh : Reply foto/gif/video, atau kirim foto/gif/video bersama caption\n\n' + 
+	'Perintah : *#toimg*\n' +
+	'Deskripsi : Merubah sticker menjadi fot\n' +
+	'Contoh : Reply sticker\n\n' +
+	'Perintah : *#tsticker* or *#tstiker*\n' +
+	'Deskripsi : Merubah teks menjadi sticker\n' +
+	'Contoh : *#tsticker Bentol Ganteng*\n\n' +
+	'Meme Commands\n' +
+	'Perintah : *#meme*\n' +
+	'Deskripsi : Menampilkan foto meme secara acak [english]\n' +
+	'Contoh : Kirim saja perintahnya\n\n' +
+	'Perintah: *!memeindo*\n' +
+	'Deskripsi : random meme images [indo]\n' +
+	'Contoh : *Kirim saja perintahnya\n\n' +
+	'Text To Speech Command\n' +
+	'Perintah : *#gtts*\n' +
+	'Deskripsi : Merubah teks menjadi audio\n' +
+	'Contoh : *#gtts [kode] [teks]*\nexample : *#gtts id Bentol Ganteng*'
+}
+
+exports.help = help
